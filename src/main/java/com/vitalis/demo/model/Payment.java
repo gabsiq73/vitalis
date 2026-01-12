63a3374dc5dd28e00c34c9a98c33ee231ba5cfca
Felipe Levi <felipegpt19@gmail.com>
2026-01-12 18:41:51 -0300

    Creating Class Payment

---
+++ src/main/java/com/vitalis/demo/model/Payment.java
@@ -0,0 +1,37 @@
+package com.vitalis.demo.model;
+
+import com.vitalis.demo.model.enums.Method;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_payment")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Payment {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue
+    @Column(name = "PAY_id")
+    private UUID id;
+
+    @Column(name = "PAY_date", nullable = false)
+    private LocalDateTime date;
+
+    @Column(name = "PAY_amount", nullable = false)
+    private Double amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Method method;
+
+    @Column(name = "PAY_notes")
+    private String notes;
+}
