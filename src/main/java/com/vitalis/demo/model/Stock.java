677bc7094981fd2a14952c68697675d65a708f5e
Felipe Levi <felipegpt19@gmail.com>
2026-01-12 10:01:36 -0300

    Creating Stock class

---
+++ src/main/java/com/vitalis/demo/model/Stock.java
@@ -0,0 +1,40 @@
+package com.vitalis.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_stock")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Stock {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "STOCK_id")
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "PROD_id")
+    private Product product;
+
+    @Column(name = "STOCK_qtd")
+    private Integer quantityInStock;
+
+    @Column(name = "STOCK_minimum")
+    private Integer minimumStock;
+
+    @Column(name = "STOCK_LastUpdated")
+    private LocalDateTime lastUpdated;
+
+    @Column(name = "STOCK_CreatedAt")
+    private LocalDateTime createdAt;
+
+}
