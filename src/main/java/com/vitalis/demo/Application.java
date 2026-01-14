15be492f38ce1adaace656a9ff2e628c331fd6ae
Gabriel Siqueira <gabriel.siqueira3000@gmail.com>
2026-01-13 22:12:58 -0300

    chore: initialize client on application startup

--- src/main/java/com/vitalis/demo/Application.java
+++ src/main/java/com/vitalis/demo/Application.java
@@ -1,13 +1,35 @@
 package com.vitalis.demo;
 
+import com.vitalis.demo.model.Client;
+import com.vitalis.demo.model.enums.ClientType;
+import com.vitalis.demo.repository.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    private final ClientRepository clientRepository;
 
+    public Application(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+
+        Client client = new Client();
+        client.setName("Gabriel");
+        client.setAddress("Rua Central");
+        client.setClientType(ClientType.RETAIL);
+
+        clientRepository.save(client);
+
+        System.out.println("Client criado com sucesso");
+    }
 }
---
+++ src/main/java/com/vitalis/demo/service/ServiceTest.java
@@ -0,0 +1,11 @@
+package com.vitalis.demo.service;
+
+import com.vitalis.demo.model.Client;
+import com.vitalis.demo.model.enums.ClientType;
+import com.vitalis.demo.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ServiceTest {
+
+
+}
