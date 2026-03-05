47f10b9aea469a42e8bf46abdf63d68a8e2d3f27
Felipe Levi <felipegpt19@gmail.com>
2026-03-05 19:33:55 -0300

    Testing entity User

--- src/main/java/com/vitalis/demo/repository/UserRepository.java
+++ src/main/java/com/vitalis/demo/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package com.vitalis.demo.repository;
 
 import com.vitalis.demo.model.Client;
+import com.vitalis.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<Client, UUID> {
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
---
+++ src/test/java/com/vitalis/demo/repository/UserRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.vitalis.demo.repository;
+
+import com.vitalis.demo.model.User;
+import com.vitalis.demo.model.enums.Role;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void saveClient(){
+        User user = new User();
+
+        user.setFirstName("Felipe");
+        user.setLastName("Sexo");
+        user.setUsername("felipeSexo");
+        user.setEmail("felipesexo@gmail.com");
+        user.setPassword("sexo123");
+        user.setUserRole(Role.ADMINISTRATOR);
+
+        userRepository.save(user);
+    }
+
+}
