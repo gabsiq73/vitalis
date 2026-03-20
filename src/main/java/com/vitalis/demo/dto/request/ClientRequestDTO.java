55b7a44c7a4bfaab9d4226033f2aea77df47497b
Felipe Levi <felipegpt19@gmail.com>
2026-03-20 07:45:38 -0300

    feat(ClientRequestDTO): add bean validations

--- src/main/java/com/vitalis/demo/dto/request/ClientRequestDTO.java
+++ src/main/java/com/vitalis/demo/dto/request/ClientRequestDTO.java
@@ -2,10 +2,16 @@ package com.vitalis.demo.dto.request;
 
 import com.vitalis.demo.model.Client;
 import com.vitalis.demo.model.enums.ClientType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record ClientRequestDTO(
+        @NotBlank(message = "Required Field!")
+        @Size(min = 2, max = 100, message = "Field out of bounds!")
         String name,
+        @Size(min = 5, max = 100, message = "Field out of bounds!")
         String address,
+        @Size(min = 5, max = 100, message = "Field out of bounds!")
         String notes,
         ClientType clientType
 ) {
