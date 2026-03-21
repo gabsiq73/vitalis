ccb5116ba8f88c6c0873619bd5dfe89f0a01851b
Gabriel Siqueira <gabriel.siqueira3000@gmail.com>
2026-03-21 16:11:24 -0300

    refactor(dto): optimize ClientRequestDTO toModel method

--- src/main/java/com/vitalis/demo/dto/request/ClientRequestDTO.java
+++ src/main/java/com/vitalis/demo/dto/request/ClientRequestDTO.java
@@ -3,25 +3,26 @@ package com.vitalis.demo.dto.request;
 import com.vitalis.demo.model.Client;
 import com.vitalis.demo.model.enums.ClientType;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record ClientRequestDTO(
         @NotBlank(message = "Required Field!")
         @Size(min = 2, max = 100, message = "Field out of bounds!")
         String name,
-        @Size(min = 5, max = 100, message = "Field out of bounds!")
+        @Size(min = 2, max = 200, message = "Field out of bounds!")
         String address,
-        @Size(min = 5, max = 100, message = "Field out of bounds!")
+        @Size(min = 5, max = 255, message = "Field out of bounds!")
         String notes,
+        @NotNull
         ClientType clientType
 ) {
-    public Client toModel(ClientRequestDTO dto){
+    public Client toModel() {
         Client client = new Client();
-        client.setName(dto.name());
-        client.setAddress(dto.address());
-        client.setNotes(dto.notes());
-        client.setClientType(dto.clientType());
-
+        client.setName(this.name());
+        client.setAddress(this.address());
+        client.setNotes(this.notes());
+        client.setClientType(this.clientType());
         return client;
     }
 }
