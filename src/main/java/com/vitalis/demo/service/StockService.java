7b15cc45d1f859c287e694fc52b9a91551d482aa
Felipe Levi <felipegpt19@gmail.com>
2026-03-22 13:13:28 -0300

    feat: add StockService class

---
+++ src/main/java/com/vitalis/demo/service/StockService.java
@@ -0,0 +1,68 @@
+package com.vitalis.demo.service;
+
+import com.vitalis.demo.exceptions.VitalisException;
+import com.vitalis.demo.model.Product;
+import com.vitalis.demo.model.Stock;
+import com.vitalis.demo.repository.StockRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class StockService {
+
+    private final StockRepository repository;
+
+    @Transactional
+    public Stock save(Stock stock){
+        validarEstoque(stock);
+        return repository.save(stock);
+    }
+
+    @Transactional(readOnly = true)
+    public Stock findById(String id){
+        UUID uuid = UUID.fromString(id);
+        return repository.findById(uuid)
+                .orElseThrow(() -> new VitalisException("Id de estoque não encontrado!"));
+    }
+
+    @Transactional(readOnly = true)
+    public Stock findByProduct(Product product){
+        return repository.findByProduct(product)
+                .orElseThrow(() -> new VitalisException("Registro de estoque não encontrado!"));
+    }
+
+    @Transactional(readOnly = true)
+    public List<Stock> findAll(){
+        return repository.findAll();
+    }
+
+    @Transactional
+    public void decreaseStock(Product product, Integer quantity){
+        Stock stock = findByProduct(product);
+        if(stock.getQuantityInStock() < quantity){
+            throw new VitalisException("Estoque insuficiente! Disponível: " + stock.getQuantityInStock());
+        }
+
+        stock.setQuantityInStock(stock.getQuantityInStock() - quantity);
+        repository.save(stock);
+    }
+
+    @Transactional
+    public void increaseStock(Product product, Integer quantity){
+        Stock stock = findByProduct(product);
+        stock.setQuantityInStock(stock.getQuantityInStock() + quantity);
+        repository.save(stock);
+    }
+    private void validarEstoque(Stock stock) {
+        if (stock.getQuantityInStock() < 0) throw new VitalisException("O saldo não pode ser negativo!");
+        if (stock.getMinimumStock() < 0) throw new VitalisException("O estoque mínimo não pode ser negativo.");
+        if (stock.getProduct() == null) throw new VitalisException("Estoque sem produto vinculado.");
+    }
+
+
+}
