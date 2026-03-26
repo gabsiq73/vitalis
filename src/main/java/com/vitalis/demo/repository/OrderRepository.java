497f1f00a7e40380d09e0de7e43f81544d50d380
Felipe Levi <felipegpt19@gmail.com>
2026-03-25 21:53:41 -0300

    feat: add Query methods to sum orders and payments

--- src/main/java/com/vitalis/demo/repository/OrderRepository.java
+++ src/main/java/com/vitalis/demo/repository/OrderRepository.java
@@ -5,8 +5,12 @@ import com.vitalis.demo.model.Order;
 import com.vitalis.demo.model.enums.OrderStatus;
 import com.vitalis.demo.model.enums.PaymentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,5 +24,13 @@ public interface OrderRepository extends JpaRepository<Order, UUID> {
 
     List<Order> findByClientAndPaymentStatusNotOrderByCreateDateAsc(Client client, PaymentStatus status);
 
+
+    @Query("SELECT SUM(i.unitPrice * i.quantity) " +
+            "FROM Order o JOIN o.items i " +
+            "WHERE o.status = :status " +
+            "AND o.deliveryDate BETWEEN :start AND :end")
+    BigDecimal sumTotalAmount(@Param("status") OrderStatus status,
+                              @Param("start") LocalDateTime start,
+                              @Param("end") LocalDateTime end);
 
 }
--- src/main/java/com/vitalis/demo/repository/PaymentRepository.java
+++ src/main/java/com/vitalis/demo/repository/PaymentRepository.java
@@ -3,8 +3,12 @@ package com.vitalis.demo.repository;
 import com.vitalis.demo.model.Order;
 import com.vitalis.demo.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -13,4 +17,8 @@ public interface PaymentRepository extends JpaRepository<Payment, UUID> {
     List<Payment> findByOrder(Order order);
 
     List<Payment> findByCreateDateBetween(LocalDate start, LocalDate end);
+
+    @Query("SELECT SUM(p.amount) FROM Payment p " +
+           "WHERE p.date BETWEEN :start AND :end")
+    BigDecimal sumTotalReceived(@Param("start")LocalDateTime start, @Param("end") LocalDateTime end);
 }
