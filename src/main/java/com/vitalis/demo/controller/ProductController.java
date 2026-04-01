1aa3bd6c7078e0624d9d8c39156817a55c15738f
Felipe Levi <felipegpt19@gmail.com>
2026-04-01 11:00:38 -0300

    refactor: standardize ProductController and ProductService with DTOs and pagination

--- src/main/java/com/vitalis/demo/controller/ProductController.java
+++ src/main/java/com/vitalis/demo/controller/ProductController.java
@@ -2,15 +2,19 @@ package com.vitalis.demo.controller;
 
 import com.vitalis.demo.dto.request.ProductRequestDTO;
 import com.vitalis.demo.dto.response.ProductResponseDTO;
+import com.vitalis.demo.dto.update.ProductUpdateDTO;
+import com.vitalis.demo.mapper.ProductMapper;
 import com.vitalis.demo.model.Product;
 import com.vitalis.demo.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -18,33 +22,43 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class ProductController {
 
-    private final ProductService service;
+    private final ProductService productService;
+    private final ProductMapper productMapper;
 
     @GetMapping
-    public ResponseEntity<List<ProductResponseDTO>> listProducts(){
-        List<ProductResponseDTO> result = service.findAll();
-        return ResponseEntity.ok(result);
+    public ResponseEntity<Page<ProductResponseDTO>> listProducts(
+            @PageableDefault(size = 10, sort = "name")Pageable pageable){
+        Page<Product> pageEntity = productService.listProducts(pageable);
+        Page<ProductResponseDTO> pageDTO = pageEntity.map(productMapper::toResponseDTO);
+        return ResponseEntity.ok(pageDTO);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ProductResponseDTO> getById(@PathVariable("id") UUID id){
-        return ResponseEntity.ok(service.findById(id));
+        return productService
+                .findByIdController(id)
+                .map(product -> {
+                    ProductResponseDTO dto = productMapper.toResponseDTO(product);
+                    return ResponseEntity.ok(dto);
+                }).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
     public ResponseEntity<ProductResponseDTO> create(@RequestBody @Valid ProductRequestDTO dto){
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(dto));
+        Product product = productService.save(productMapper.toEntity(dto));
+        ProductResponseDTO responseDTO = productMapper.toResponseDTO(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Void> update(@PathVariable("id") UUID id, @RequestBody @Valid ProductRequestDTO dto){
-        service.update(id, dto);
+    public ResponseEntity<Void> update(@PathVariable("id") UUID id, @RequestBody @Valid ProductUpdateDTO dto){
+        productService.update(id, dto);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") UUID id){
-        service.delete(id);
+        productService.delete(id);
         return ResponseEntity.noContent().build();
     }
 
--- src/main/java/com/vitalis/demo/service/ProductService.java
+++ src/main/java/com/vitalis/demo/service/ProductService.java
@@ -2,15 +2,20 @@ package com.vitalis.demo.service;
 
 import com.vitalis.demo.dto.request.ProductRequestDTO;
 import com.vitalis.demo.dto.response.ProductResponseDTO;
+import com.vitalis.demo.dto.update.ProductUpdateDTO;
 import com.vitalis.demo.infra.exception.BusinessException;
+import com.vitalis.demo.mapper.ProductMapper;
 import com.vitalis.demo.model.Product;
 import com.vitalis.demo.repository.ClientPriceRepository;
 import com.vitalis.demo.repository.ProductRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -20,19 +25,25 @@ public class ProductService {
     private final ProductRepository repository;
     private final ClientPriceRepository clientPriceRepository;
     private final StockService stockService;
+    private final ProductMapper productMapper;
 
 
     @Transactional(readOnly = true)
-    public ProductResponseDTO findById(UUID id){
-        Product product = findEntityById(id);
-        return ProductResponseDTO.fromEntity(product);
+    public Optional<Product> findByIdController(UUID id){
+        return repository.findById(id);
     }
 
     @Transactional(readOnly = true)
-    public List<ProductResponseDTO> findAll(){
-        return repository.findAll().stream()
-                .map(ProductResponseDTO::fromEntity)
-                .toList();
+    public Page<Product> listProducts(Pageable pageable){
+        return repository.findAll(pageable);
+    }
+
+    @Transactional
+    public Product save(Product product){
+        Product savedProduct = repository.save(product);
+        stockService.createInitialStock(savedProduct);
+
+        return savedProduct;
     }
 
     @Transactional
@@ -41,39 +52,12 @@ public class ProductService {
         repository.delete(product);
     }
 
-    @Transactional
-    public ProductResponseDTO save(ProductRequestDTO dto){
-        Product product = new Product();
-        product.setName(dto.name());
-        product.setBasePrice(dto.basePrice());
-        product.setType(dto.type());
-
-        Product savedProduct = repository.save(product);
-
-        stockService.createInitialStock(product);
-
-        return ProductResponseDTO.fromEntity(savedProduct);
-    }
 
     @Transactional
-    public void update(UUID id, ProductRequestDTO dto){
-        Product foundProduct = findEntityById(id);
-
-        if (dto.name() != null && !dto.name().isBlank()) {
-            foundProduct.setName(dto.name());
-        }
-
-        if (dto.basePrice() != null) {
-            foundProduct.setBasePrice(dto.basePrice());
-        }
-
-        if (dto.type() != null) {
-            foundProduct.setType(dto.type());
-        }
-
-        // 3. Salva a entidade atualizada
-        repository.save(foundProduct);
-
+    public void update(UUID id, ProductUpdateDTO dto){
+        Product product = findEntityById(id);
+        productMapper.updateEntityFromDto(dto, product);
+        repository.save(product);
     }
 
     public Product findEntityById(UUID id) {
