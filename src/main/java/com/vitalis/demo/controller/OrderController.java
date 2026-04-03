c8694e910da87b8529a3fbe0d4c11c935f864df5
Gabriel Siqueira <gabriel.siqueira3000@gmail.com>
2026-04-03 18:00:32 -0300

    feat(controller): implement OrderController with listAll and Create

---
+++ src/main/java/com/vitalis/demo/controller/OrderController.java
@@ -0,0 +1,40 @@
+package com.vitalis.demo.controller;
+
+import com.vitalis.demo.dto.request.OrderRequestDTO;
+import com.vitalis.demo.dto.response.OrderResponseDTO;
+import com.vitalis.demo.mapper.OrderMapper;
+import com.vitalis.demo.model.Order;
+import com.vitalis.demo.service.OrderService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+    private final OrderMapper orderMapper;
+
+    @GetMapping
+    public ResponseEntity<Page<OrderResponseDTO>> listOrders(
+            @PageableDefault(size = 10, sort = "createDate", direction = org.springframework.data.domain.Sort.Direction.DESC) Pageable pageable) {
+        Page<Order> pageEntity = orderService.listOrders(pageable);
+        Page<OrderResponseDTO> pageDTO = pageEntity.map(orderMapper::toResponseDTO);
+        return ResponseEntity.ok(pageDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderResponseDTO> create(@RequestBody @Valid OrderRequestDTO dto) {
+        Order order = orderService.createOrder(dto);
+        OrderResponseDTO response = orderMapper.toResponseDTO(order);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
